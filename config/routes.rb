6cdb3ff6189747cbaab5d34ce324a7c0150c0afe
Kevin Ullmann <kev.ullmann@gmail.com>
2016-07-08 16:15:03 -0400

    use althello

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HelloApp::Application.routes.draw do
-  root 'application#hello'
+  root 'application#althello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
